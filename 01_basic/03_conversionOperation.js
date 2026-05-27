493d5aba1e732f94dea57e3c829c64f92679a504
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-05-27 06:16:38 +0000

    conversionDataType

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = undefined
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+//let conString = String(score)
+//console.log(typeof conString)
+//console.log(conString)
+
+let value = Number(score)
+console.log(typeof value)
+console.log(value)
+
+let isloggedIn =1
+
+let booleanIsLoggedId = Boolean (isloggedIn)
+console.log( booleanIsLoggedId)
