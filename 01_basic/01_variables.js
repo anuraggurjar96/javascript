935b5b720d762eb247b3004d2a1af73136959129
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-05-27 05:45:07 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12387
+let accountEmail = "gurjaranurag67@gmail.com"
+var accountPassword = "12u73"
+accountCity = "Jaipur"
+var accountState;
+//console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.log(accountEmail)
+/*
+perfer not to use var brcause of issue
+in block scope and function scop
+*/
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer version
+//alert(3+7) // we are using node.js,not browser
+
+console.log(3+7)
+
+console.log("Anurag")
+
+let name = "anurag"
+let age = 20
+let isloggedIn = true
+
+//bigint
+//string = ""
+//bollean => true/false
+// null => stanalone values
+
+// undefined => 
+    // symbol => unique
+//object 
+
+console.log(typeof null) //object
+console.log(typeof undefine) // undefine
