fbd7dd0f46df4734f5bc0570faa7a7d70dffb59e
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-05-28 07:40:02 +0000

    datatupe-summary

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,25 @@
+// basic conversion
+
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)// is case me null = 0 asume 
+console.log(null <= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+ // undefined case all case false .....
+
+// === use this for equality and data type both 
+
+console.log("2" === 2)
---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,38 @@
+// primitive datatype 
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+const bigNumber = 3462465475668n
+
+
+
+
+// Reference (Non Primitive)
+// Array, Object, Functions
+
+const Array = ["Anurag","Anu","Abhishek"]
+
+let myobj = {
+    name: "Anurag",
+    age: 20,
+}
+
+    const myfunction = function()
+    {
+        console.log("Hello Anurag");
+
+    }
+console.log(typeof scoreValue);
+console.log(typeof myfunction);
+
