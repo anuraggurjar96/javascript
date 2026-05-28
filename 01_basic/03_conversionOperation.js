f4320808712e71bcce1b87e7c360b05c32bd2ac4
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-05-28 05:04:15 +0000

    conversionOperation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,17 +1,40 @@
-let score = undefined
+let value = 55
 
-//console.log(typeof score)
-//console.log(typeof (score))
+let negValue = -value
 
-//let conString = String(score)
-//console.log(typeof conString)
-//console.log(conString)
+//console.log(negValue)
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2); power kw liye
+//console.log(2/2);
+//console.log(2%2);
 
-let value = Number(score)
-console.log(typeof value)
-console.log(value)
 
-let isloggedIn =1
+let str1 = "anurag"
+let str2 = " gurjar"
 
-let booleanIsLoggedId = Boolean (isloggedIn)
-console.log( booleanIsLoggedId)
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2") //
+// console.log("1" + "2")
+// console.log(1 + 2 +"2") // pahale operation then string asume 
+// console.log("1" + 2 + 3)// stating me str hone complet ko string asume kar leta h 
+
+// console.log(true)
+// console.log(+true)
+//console.log(true+)// not posible
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 3+3
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter)
+
+
+
+
