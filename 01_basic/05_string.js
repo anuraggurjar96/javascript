009bd8400b24bec4c77756332bd62eceb11da56c
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-05-29 10:41:26 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,35 @@
+const name = "anurag"
+const repCount = 50
+
+//console.log('Hello my nmae is ${name} and my rep count is ${repoCount}')
+
+const gameName = new String('anuraggurjar')
+
+//console.log(gameName[0])
+
+//console.log(gameName.__proto__)
+
+//console.log(gameName.length)
+//console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('g'))
+
+const newString = gameName.substring(0,6)
+
+console.log(newString)
+
+const anotherString = gameName.slice(-10,6)
+const anotherStringone = gameName.slice(-10,-6)
+
+console.log(anotherString)
+console.log(anotherStringone)
+
+const String1 ="   anurag    "
+console.log(String1)
+console.log(String1.trim())
+
+const url = "alphapixeloffical087@gmail.com"
+console.log(url.replace('offical','#'))
+
+console.log(url.includes('sundar'))
