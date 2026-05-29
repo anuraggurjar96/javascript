61f05c0aa90cac9dd4bdf909f31d22fb161679b8
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-05-29 10:42:12 +0000

    datesin

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,45 @@
+  // dates
+   //let myDate = new Date()
+
+//console.log(myDate.toString())
+
+//console.log(myDate.toDateString())
+
+//console.log(myDate.toLocaleString())
+
+//console.log(typeof myDate)
+
+//let myCreatedDate1 = new Date(2026, 5, 29)
+//console.log(myCreatedDate1)
+
+//let myCreatedDate2 = new Date(2026, 0, 29)
+//console.log(myCreatedDate2)
+
+//let myCreatedDate3 = new Date(2026, 5, 29, 4, 9)
+//console.log(myCreatedDate3)
+
+let myCreatedDate = new Date("2026-05-29")
+console.log(myCreatedDate.toLocaleString())
+
+// TimeStamp......
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp)
+//console.log(myCreatedDate.getTime())
+
+//console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDay())
+
+// 
+newDate.toLocaleString('default', {weekday: "long"
+
+})
+
+
+
