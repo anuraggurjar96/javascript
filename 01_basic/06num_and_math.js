565ed218b9145ac001b5750fbc0c88429f9a814c
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-05-29 10:41:53 +0000

    num_Math

---
+++ 01_basic/06num_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(4))
+
+const otherNumber = 123.8966
+//console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//+++++++++Maths+++++++++++++
+
+console.log(Math); // M is capital als
+console.log(Math.abs(-5))
+
+console.log(Math.round(4.6))
+console.log(Math.ceil(5.3))
+console.log(Math.floor(4.9))
+
+console.log(Math.min(3,4,5,6))
+console.log(Math.max(7,8,4,3))
+
+console.log(Math.random())
+console.log(Math.random()*10 + 1)
+console.log(Math.random()*10 + 1)
+
+const min = 10
+const max =20
+
+console.log(Math.floor(Math.random()*(max-min + 1)) +min)
+
+
+
+
