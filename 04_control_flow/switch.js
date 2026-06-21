43723ebd303f9397e6ff4abba3960cdd860d1b9c
anuraggurjar96 <gurjaranurag67@gmail.com>
2026-06-21 06:04:38 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,39 @@
+// switch (key)
+// {
+//     //case values
+
+//     // breal;
+
+//     //default
+
+//     break;
+
+// }
+
+const month = 5
+
+switch (month)
+{
+    //case values
+     case 1:
+        console.log("January");
+        break;
+     case 2:
+        consol.elog("feb");
+        break;
+     case 3:
+        console.log("March");
+        break;
+     case 4:
+         console.log("Apiral");
+        break;
+
+        // use for break to control and stop the flow of coe 
+        
+
+    default:
+      console.log("Not found..")
+        break;
+
+    
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,47 @@
+const UserEmail =[]
+
+if(UserEmail)
+{
+    //console.log("Got user email");
+
+}
+
+else{
+    //console.log("Dont have user email");
+
+    
+}
+
+// falsy values 
+
+
+// false,0,-0, BigInt 0n, "",null,undefined ,NaN
+
+// truthy values...
+//"0",'false', " ";[],{ }, function(){}
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0)
+{
+    //console.log("Object is empty")
+
+}
+
+// Nullish caolescing operator (??) null undefine
+
+let val1;
+val1 = 5 ??  10
+val1 = null ?? 10
+val = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1)
+
+// Terniary Operator....
+
+// condition ? true : false use simple if _ else op
+
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80")
